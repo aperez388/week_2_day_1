92cfaf2e15812563c224aa51e21a017d21251e71
Perez <aperez388@cps.k12.il.us>
2024-09-04 15:12:32 -0500

    sps

--- week2.py
+++ week2.py
@@ -1,9 +1,12 @@
 # ask someone what day it is
-day = input("What day is it?")
-print("The day is " + day + ".")
+# day = input("What day is it?")
+# print("The day is " + day + ".")
 # ask them their favorite holiday
-holiday = input("What is your favorite holiday?")
-print("Your favorite holiday is " + holiday + ".")
+# holiday = input("What is your favorite holiday?")
+# print("Your favorite holiday is " + holiday + ".")
 # print out the day and their favorite holiday in a sentence
-print("Today is " + day + " and your favorite holiday is " + holiday + ".")
+# print("Today is " + day + " and your favorite holiday is " + holiday + ".")
 # making a change to the file
+# string slicing
+word = "hello"
+print(word[1:4])
